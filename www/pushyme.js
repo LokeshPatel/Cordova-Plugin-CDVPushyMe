07e6acbae293dd7fd5cc29ee055e3e98da9e0700
Lokesh Patel <LokeshPatel@users.noreply.github.com>
2016-05-17 14:01:43 +0530

    Update

--- www/pushyme.js
+++ www/pushyme.js
@@ -1,10 +1,24 @@
 var exec = require('cordova/exec');
 var pushyMe = {
-     pushyMeID :function(callbacksucess,callbackfail) {
+  pushyMeID :function(callbacksucess,callbackfail) {
      exec(callbacksucess,callbackfail,"CDVPlushyMePlugin", "pushyMeTokenID", []);
    },
-    pushyMeInstall :function(callbacksucess,callbackfail) {
+  pushyMeInstall :function(callbacksucess,callbackfail) {
     exec(callbacksucess,callbackfail,"CDVPlushyMePlugin", "installPlugin", []);
-  }
+  },
+  pushyMeReadAllMessages :function(callbacksucess,callbackfail) {
+      exec(callbacksucess,callbackfail,"CDVPlushyMePlugin", "getAllMessages", []);
+    },
+  pushyMeClearAllMessages :function(callbacksucess,callbackfail) {
+       exec(callbacksucess,callbackfail,"CDVPlushyMePlugin", "clearMessageFromStore", []);
+     },
+ pushyMeClearByMessagesId :function(callbacksucess,callbackfail,messageID) {
+      if(messageID == "")
+      {
+        callbackfail("Insert Message Id");
+        return;
+        }
+        exec(callbacksucess,callbackfail,"CDVPlushyMePlugin", "clearMessageByIdFromStore", [messageID]);
+    }
 };
 module.exports = pushyMe;
